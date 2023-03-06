47e14d14f7744925f5e2c7a1c640da970c6871d0
gabbar117 <1by19cs117@bmsit.in>
2023-03-06 20:33:24 +0530

    Ver1.0

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+
+const toggleElem = function (elem) { elem.classList.toggle("active"); }
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", function () {
+    toggleElem(navbar);
+    toggleElem(overlay);
+  });
+}
+
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    header.classList.add("header-anim");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+    header.classList.remove("header-anim");
+  }
+});
